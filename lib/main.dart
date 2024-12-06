783e947c5ade7a1d155e61cfae8cdbd5389390d3
Eduardo Pires <eduardopires.dev@outlook.com>
2024-12-06 10:24:09 -0300

    Divisão das telas e adição de tela de cadastro

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'tela_login.dart';
+import 'tela_cadastro.dart';
+
+void main() {
+  runApp(const FluentifyApp());
+}
+
+class FluentifyApp extends StatelessWidget {
+  const FluentifyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const LoginScreen(),
+      routes: {
+        '/signup': (context) => const SignupScreen(),
+      },
+    );
+  }
+}
+
